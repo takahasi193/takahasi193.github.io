36559589481163e0347c7a223739ec46af89dbd4
takahasi193 <875110152@qq.com>
2025-09-03 12:18:53 +0800

    Site updated: 2025-09-03 12:18:51

---
+++ js/title.js
@@ -0,0 +1,18 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '置いていかないて~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = 'おかえり～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
+
